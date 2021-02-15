060d7bf16305130ffc7c40d7222c1e73a1a2bdb1
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-15 17:25:41 +0100

    Create ArrayMaxHeap tests and class
    Add tests for constructors, indexers and properties. Implement default
    constructor initializing members, indexer boundary check, add more
    rules regarding .gitignore - debugging and executables of tests.

---
+++ CsharpHeap/ArrayMaxHeap.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHeap
+{
+    public class ArrayMaxHeap<T> : Heap<T> where T : IComparable
+    {
+        #region Properties
+        private T[] Items;
+        public override int Count { get; protected set; }
+        public override int Level { get; protected set; }
+        public override int MaxSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #endregion
+
+        #region Ctors and indexers
+        public ArrayMaxHeap()
+        {
+            Count = Level = 0;
+            Items = new T[1];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if(index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return Items[index];
+            }
+        }
+        #endregion
+
+        public override T Child(ChildSide side)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Current()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MoveOn(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Parent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Push(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Push(T obj, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Seek(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CsharpHeapTests/ArrayMaxHeapTests.cs
+++ CsharpHeapTests/ArrayMaxHeapTests.cs
@@ -4,12 +4,43 @@ using System;
 
 namespace CsharpHeapTests
 {
+
     [TestClass]
     public class ArrayMaxHeapTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Create_NewInstance_InitializesValuesCorrectly()
         {
+            ArrayMaxHeap<int> arrayMaxHeap = new ArrayMaxHeap<int>();
+
+            int count = arrayMaxHeap.Count;
+            int level = arrayMaxHeap.Level;
+
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(0, level);
+        }
+
+        [TestMethod]
+        public void Indexer_IndexOutOfRange_ThrowsException()
+        {
+            ArrayMaxHeap<int> arrayMaxHeap = new ArrayMaxHeap<int>();
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                _ = arrayMaxHeap[1];
+            });
+        }
+
+        [TestMethod]
+        public void Indexer_CorrectIndex_ReturnsElement()
+        {
+            ArrayMaxHeap<int> arrayMaxHeap = new ArrayMaxHeap<int>();
+            int anyInteger = 1;
+
+            arrayMaxHeap.Push(anyInteger);
+            int result = arrayMaxHeap[0];
+
+            Assert.AreEqual(1, result);
         }
     }
 }
