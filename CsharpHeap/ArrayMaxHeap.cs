8edb350cf97b53cf83b66d88125c3a831a8bcb3e
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-15 17:52:47 +0100

    Implement full ArrayMaxHeap functionality
    Add required methods compatible with modelled UML class diagram.

--- CsharpHeap/ArrayMaxHeap.cs
+++ CsharpHeap/ArrayMaxHeap.cs
@@ -35,6 +35,119 @@ namespace CsharpHeap
         }
         #endregion
 
+        private Boolean HasElementAtIndex(int index)
+        {
+            return index < Count ? true : false;
+        }
+
+        public int GetLeftChildIndex(int parentIndex)
+        {
+            if (HasElementAtIndex(parentIndex))
+            {
+                return 2 * parentIndex + 1;
+            }
+            throw new IndexOutOfRangeException("Parent at given index does not exist.");
+        }
+
+        public int GetRightChildIndex(int parentIndex)
+        {
+            if (HasElementAtIndex(parentIndex))
+            {
+                return 2 * parentIndex + 2;
+            }
+            throw new IndexOutOfRangeException("Parent at given index does not exist.");
+        }
+
+        public int GetParentIndex(int childIndex)
+        {
+            if (HasElementAtIndex(childIndex))
+            {
+                return (int)Math.Floor((childIndex - 1) / 2.0);
+            }
+            throw new IndexOutOfRangeException("Child at given index does not exist.");
+        }
+
+        public Boolean HasLeftChild(int parentIndex)
+        {
+            return GetLeftChildIndex(parentIndex) < Count;
+        }
+
+        public Boolean HasRightChild(int parentIndex)
+        {
+            return GetRightChildIndex(parentIndex) < Count;
+        }
+
+        public Boolean HasParent(int childIndex)
+        {
+            return GetParentIndex(childIndex) >= 0;
+        }
+
+        public T GetLeftChild(int parentIndex)
+        {
+            return Items[GetLeftChildIndex(parentIndex)];
+        }
+
+        public T GetRightChild(int parentIndex)
+        {
+            return Items[GetRightChildIndex(parentIndex)];
+        }
+
+        public T GetParent(int childIndex)
+        {
+            return Items[GetParentIndex(childIndex)];
+        }
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            (Items[firstIndex], Items[secondIndex]) = (Items[secondIndex], Items[firstIndex]);
+        }
+
+        public override T Seek(int index)
+        {
+            if (index < 0 || index >= Count - 1)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+            return Items[index];
+        }
+
+        public override T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Heap does not contain elements to be popped");
+            }
+            T root = Items[0];
+            Items[0] = Items[Count - 1];
+            Count--;
+            HeapifyDown();
+            UpdateHeapLevel();
+            return root;
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                int biggerChildIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetRightChild(index).CompareTo(GetLeftChild(index)) > 0)
+                {
+                    biggerChildIndex = GetRightChildIndex(index);
+                }
+
+                if (Items[index].CompareTo(Items[biggerChildIndex]) > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, biggerChildIndex);
+                }
+                index = biggerChildIndex;
+            }
+        }
+
         public override T Child(ChildSide side)
         {
             throw new NotImplementedException();
@@ -55,22 +168,53 @@ namespace CsharpHeap
             throw new NotImplementedException();
         }
 
-        public override T Pop()
+        public override int Push(T item)
         {
-            throw new NotImplementedException();
+            ExpandHeap();
+            Items[Count] = item;
+            Count++;
+            try
+            {
+                HeapifyUp();
+                UpdateHeapLevel();
+                return 0; // return 0 whether HeapifyUp was successful
+            }
+            catch
+            {
+                return 1; // return 1 whether HeapifyUp thrown an error
+            }
         }
 
-        public override int Push(T obj)
+        private void UpdateHeapLevel()
         {
-            throw new NotImplementedException();
+            if (Count == 0) Level = 0;
+            else Level = (int)Math.Floor(Math.Log(Count, 2));
+        }
+
+        private void HeapifyUp()
+        {
+            int index = Count - 1;
+            while (HasParent(index) && GetParent(index).CompareTo(Items[index]) < 0)
+            {
+                Swap(GetParentIndex(index), index);
+                index = GetParentIndex(index);
+            }
+        }
+
+        private void ExpandHeap()
+        {
+            if (Count == Items.Length)
+            {
+                T[] expandedArray = new T[2 * Items.Length];
+                for (int i = 0; i < Count; i++)
+                {
+                    expandedArray[i] = Items[i];
+                }
+                Items = expandedArray;
+            }
         }
 
         public override int Push(T obj, int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override T Seek(int index)
         {
             throw new NotImplementedException();
         }
